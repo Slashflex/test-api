dd867ac6954c70218d3996571ace22e1d1875952
slashflex <pro.davidsaoud@gmail.com>
2020-04-30 10:58:14 +0200

    add: ManyToOne

---
+++ src/Entity/Product.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Offer;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ * @ApiResource
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Notice the "cascade" option below, this is mandatory if you want Doctrine to automatically persist the related entity
+     * 
+     * @ORM\OneToMany(targetEntity="Offer", mappedBy="product", cascade={"persist"})
+     */
+    private $offers;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    public function __construct()
+    {
+        $this->offers = new ArrayCollection(); // Initialize $offers as a Doctrine collection
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // Adding both an adder and a remover as well as updating the reverse relation is mandatory
+    // if you want Doctrine to automatically update and persist (thanks to the "cascade" option) the related entity
+    public function addOffer(Offer $offer): void
+    {
+        $offer->product = $this;
+        $this->offers->add($offer);
+    }
+
+    public function removeOffer(Offer $offer): void
+    {
+        $offer->product = null;
+        $this->offers->removeElement($offer);
+    }
+
+    public function getOffers(): Collection
+    {
+        return $this->offers;
+    }
+
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
